025147f6fb6d16290fc72e994028d47593fb0da3
WAGuerra <wladimir.guerra@gmail.com>
2020-08-31 10:25:56 -0300

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020 by Wladimir Guerra. All rights reserved.
+ */
+
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 4,
+}
